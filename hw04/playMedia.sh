ae6de2a90e683198a0e7d4f6351bf071e1bc342e
Sophia Harrison <harriss1@rose-hulman.edu>
2023-01-10 18:25:01 +0000

    video done

--- hw04/playMedia.sh
+++ hw04/playMedia.sh
@@ -8,25 +8,17 @@ export SDL_VIDEODRIVER=fbcon
 export SDL_FBDEV=/dev/fb0
 
 # display image
-#sudo fbi -noverbose -T 1 -a dog-beagle-portrait.jpg    
+sudo fbi -noverbose -T 1 -a dog-beagle-portrait.jpg
+
+# rotate and display
+TMP_FILE=/tmp/rotate.png
+convert dog-beagle-portrait.jpg -rotate 90 $TMP_FILE
+sudo fbi -noverbose -T 1 -a $TMP_FILE
+
+# text on image and display
+TMP_FILE=/tmp/text.png
+convert dog-beagle-portrait.jpg -gravity Center  -fill green -pointsize 100 -annotate 0 'BEAGLE BONE ROCKS' $TMP_FILE
+sudo fbi -noverbose -T 1 -a $TMP_FILE
 
 # display video
-#mplayer -vo fbdev2 -nolirc -framedrop fishies.mp4
-
-# Display text:
-SIZE=80x80
-TMP_FILE=/tmp/frame.png
-
-# From: http://www.imagemagick.org/Usage/text/
-convert -background blue -fill blue -font Times-Roman -pointsize 24 \
-     -size $SIZE \
-     label:'This is text\nhello\n-sophia' \
-     -draw "text 0,200 'Bottom Text'" \
-     $TMP_FILE
-
-#sudo fbi -noverbose -T 1 $TMP_FILE
-convert dog-beagle-portrait.jpg -gravity south \
-          -stroke '#000C' -strokewidth 2 -annotate 0 'Faerie Dragon' \
-          -stroke  none   -fill white    -annotate 0 'Faerie Dragon' \
-          $TMP_FILE
-sudo fbi -noverbose -T 1 $TMP_FILE
+mplayer -vo fbdev2 -nolirc -framedrop fishies.mp4
