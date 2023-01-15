df8af703cd4bee546db3da8f94f33880ce5c9bdd
Sophia Harrison <harriss1@rose-hulman.edu>
2023-01-15 18:17:57 +0000

    small bug fix caught aftr grading

--- hw04/setuptemp.sh
+++ hw04/setuptemp.sh
@@ -2,7 +2,7 @@
 # run this to be able to run tempReadDriver.sh
 # sets up permissions and the devices
 #
-
+cd /sys/class/i2c-adapter/i2c-2/
 sudo chown debian:debian *
 echo tmp101 0x48 > new_device
 echo tmp101 0x4a > new_device
